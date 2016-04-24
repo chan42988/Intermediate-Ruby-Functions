71d3da0fb953528158a37371d5be7af61bdd6c1c
Christopher <chan42988@gmail.com>
2016-04-24 18:54:57 -0400

    understanding factorial

--- inter.rb
+++ inter.rb
@@ -3,13 +3,14 @@
 # def rotate
 
 # 	a = [1,2,3,4,5]
+	#shift four times and shovel into the array
 # 	a << a.shift(4)
 # 	a
 
 # end
 # puts rotate
 
-#solution 2
+#array rotate solution 2
 # def rotate
 # 	#set array to string
 # 	array = ['cat','dog','mouse','shoe']
@@ -20,12 +21,24 @@
 # end
 # 	puts rotate
 
-#math factorial 
-#from what I can understand, 1..n means taking any number and injecting it into the first block. product * n = taking the result and multiplying into whatever integer n may be.
-def fact(n)
-	(1..n)
-
+#math factorial solution 2
+#we want the multiplication from 1 to n to get the factorial
 def factorial(n)
-  (1..n).inject {|product, n| product * n }
+  total = 1
+  #multiply each number from 1 to n 
+  (1..n).each do |n|
+  	#takes the total and multiplies n number of times until n = n 
+    total *= n   
+  end
+  total
 end
-puts factorial(5)
+
+puts factorial()
+
+#factorial solution 2
+#from what I can understand, 1..n means taking 1 through n and injecting it into the first block. product * n = taking the result and multiplying into whatever integer n may be.
+# def factorial(n)
+#   (1..n).inject {|product, n| product * n }
+# end
+
+# puts factorial()
