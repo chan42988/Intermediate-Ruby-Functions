4ee1ca5a7dd2b197521bb1891d209e9e14b9b663
Christopher <chan42988@gmail.com>
2016-04-24 16:39:45 -0400

    another solution to rotate

--- inter.rb
+++ inter.rb
@@ -1,19 +1,22 @@
 #array rotate
+#solution 1
+# def rotate
 
+# 	a = [1,2,3,4,5]
+# 	a << a.shift(4)
+# 	a
+
+# end
+# puts rotate
+
+#solution 2
 def rotate
-
 	#set array to string
-	# array2 = ['cat','dog','mouse','shoe']
-	array = [1,2,3,4,5]
+	array = ['cat','dog','mouse','shoe']
 	#shift removes first element
 	#push appends it to the end
 	array.push(array.shift)
 	array.push(array.shift)
-	array.push(array.shift)
-	array.push(array.shift)
-	# array2.push(array2.shift)
-	# array2.push(array2.shift)
-
 end
 	puts rotate
 
